6008a598b9424a8220855ea41801c47090f3cc9c
srgg6701 <srgg.next@gmail.com>
2025-08-09 10:51:13 +0300

    Customizing navbar visibility

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,14 @@
+"use client";
 import {
   Navbar as HeroUINavbar,
   NavbarContent,
   NavbarMenu,
   NavbarMenuToggle,
   NavbarItem,
-  NavbarMenuItem,
 } from "@heroui/navbar";
-import { Button } from "@heroui/button";
+//import { Button } from "@heroui/button";
 import { Kbd } from "@heroui/kbd";
-import { Link } from "@heroui/link";
+//import { Link } from "@heroui/link";
 import { Input } from "@heroui/input";
 import { link as linkStyles } from "@heroui/theme";
 import NextLink from "next/link";
@@ -17,8 +17,11 @@ import clsx from "clsx";
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { SearchIcon } from "@/components/icons";
+import { useUserStore } from "@/lib/store/userStore";
 
 export const Navbar = () => {
+  const { isAuthenticated } = useUserStore();
+
   const searchInput = (
     <Input
       aria-label="Search"
@@ -40,41 +43,51 @@ export const Navbar = () => {
     />
   );
 
-  const menuList = () =>
-    siteConfig.navItems.map((item) => (
-      <NavbarItem key={item.href}>
-        <NextLink
-          className={clsx(
-            linkStyles({ color: "foreground" }),
-            "data-[active=true]:text-primary data-[active=true]:font-medium",
-          )}
-          color="foreground"
-          href={item.href}
-        >
-          {item.label}
-        </NextLink>
-      </NavbarItem>
-    ));
+  const menuList = () => {
+    return siteConfig.navItems.map((item) => {
+      if (item.protected && !isAuthenticated) {
+        return null;
+      }
+      /* if (item.label === "Login" && isAuthenticated) {
+        return null;
+      } */
+      if (item.label === "Logout" && !isAuthenticated) {
+        return null;
+      }
+      return (
+        <NavbarItem key={item.href}>
+          <NextLink
+            className={clsx(
+              linkStyles({ color: "foreground" }),
+              "data-[active=true]:text-primary data-[active=true]:font-medium",
+            )}
+            color="foreground"
+            href={item.href}
+          >
+            {item.label}
+          </NextLink>
+        </NavbarItem>
+      );
+    });
+  };
 
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="center">
         <div className="hidden gap-[10%] lg:flex w-full">
-          <ul className="gap-4 flex items-center">
-            {menuList()}
-          </ul>
-          {searchInput}
+          <ul className="gap-4 flex items-center">{menuList()}</ul>
+          {isAuthenticated && searchInput}
           <ThemeSwitch />
         </div>
       </NavbarContent>
       <NavbarContent className="basis-1 pl-4 lg:hidden" justify="end">
         <ThemeSwitch />
-        <NavbarMenuToggle />
+        {isAuthenticated && <NavbarMenuToggle />}
       </NavbarContent>
-      <NavbarMenu>
+      {isAuthenticated && <NavbarMenu>
         {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">{menuList()}</div>
-      </NavbarMenu>
+      </NavbarMenu>}
     </HeroUINavbar>
   );
 };
--- config/site.ts
+++ config/site.ts
@@ -1,28 +1,28 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: 'Next.js + HeroUI',
-  description: 'Make beautiful websites regardless of your design experience.',
+  name: 'Moneliq Easy Task',
+  description: 'Frontend Assessment for a potential Employer',
   navItems: [
     {
       label: 'Dashboard',
       href: '/dashboard',
+      protected: true,
     },
     {
       label: 'Balances',
       href: '/balances',
+      protected: true,
     },
     {
       label: 'Currencies',
       href: '/currencies',
-    },
-    {
-      label: 'Login',
-      href: '/login',
+      protected: true,
     },
     {
       label: 'Logout',
       href: '/logout',
+      protected: true,
     },
   ],
 };
